e08ae7412466b79a80b1edd9fdab4aec9b27c95d
Braffolk <estoniascape@gmail.com>
2022-03-27 19:58:05 +0400

    updated ver

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def version_libs(libs, precisions, versions):
 INSTALL_REQUIRES = version_libs(libs, precisions, versions)
 
 setup(name='weightipy',
-      version='0.0.3',
+      version='0.1.0',
       author='Remi Sebastian Kits',
       author_email='kaitumisuuringute.keskus@gmail.com',
       packages=find_packages(exclude=['tests']),
