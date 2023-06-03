4a8049ccc0d73ce241fc8717ec9d156a3ae25007
braffolk <estoniascape@gmail.com>
2023-06-03 23:39:57 +0300

    added utility functions for simple schema generation from df or dict

--- tests/test_a.py
+++ tests/test_a.py
@@ -1,1 +1,30 @@
-pass
+import unittest
+
+import pandas as pd
+
+from weightipy.rim import Rim
+from weightipy import weight_dataframe, schema_from_dict, weighting_efficiency, schema_from_df
+
+class TestUtility(unittest.TestCase):
+
+    def test_schema_from_distribution(self):
+        df = pd.read_csv("./tests/Example Data (A).csv")
+        dist = {
+            "gender": {
+                1: 0.5,
+                2: 0.5
+            }
+        }
+        schema = schema_from_dict(dist)
+        df = weight_dataframe(df, schema)
+        eff = weighting_efficiency(df["weights"])
+        self.assertGreater(eff, 10)
+        self.assertLess(eff, 100)
+
+    def test_schema_from_df(self):
+        df = pd.read_csv("./tests/Example Data (A).csv")
+        df["n"] = 1
+        schema = schema_from_df(df, cols_weighting=["gender", "age"], col_freq="n")
+        df = weight_dataframe(df, schema)
+        eff = weighting_efficiency(df["weights"])
+        self.assertLess(abs(eff - 100.0), 1.0)
--- weightipy/__init__.py
+++ weightipy/__init__.py
@@ -1,3 +1,6 @@
+import random
+from typing import Dict, Union, Optional, List, Any
+
 import pandas as pd
 
 from weightipy.rim import Rim
@@ -8,6 +11,17 @@ from weightipy.weight_engine import WeightEngine
 def weight_dataframe(df: pd.DataFrame, scheme: Rim, weight_column="weights") -> pd.DataFrame:
     df = df.copy()
     df["__identity__"] = range(len(df))
+
+    # Convert weight columns to categories
+    cols_weight = []
+    for _, group in scheme.groups.items():
+        for d in group["targets"]:
+            col = list(d.keys())[0]
+            cols_weight.append(col)
+
+    for col in cols_weight:
+        df[col] = df[col].astype("category")
+
     engine = WeightEngine(data=df)
     engine.add_scheme(scheme=scheme, key="__identity__", verbose=False)
     engine.run()
@@ -22,3 +36,82 @@ def weighting_efficiency(weights: pd.Series) -> float:
     sws = (weights.sum()) ** 2
     ssw = (weights ** 2).sum()
     return (sws / len(weights)) / ssw * 100
+
+
+def schema_from_dict(
+        distributions: Dict[str, Dict[Any, Union[float, int]]],
+        name: Optional[str] = None,
+        rim_params: Optional[Dict] = None
+) -> Rim:
+    """
+    Create a Rim schema from a dictionary of distributions. The dictionary should be in the format
+    {dimension: {value: proportion}}. The proportions should be in percentages, e.g. 0.5 for 50%.
+
+    This function is useful for simple schemas, where filters are not needed. For more complex schemas,
+    use the Rim class directly.
+
+    Args:
+        distributions: Dictionary of distributions
+        name: Name of the schema
+        rim_params: Parameters for the Rim class
+
+    Returns:
+
+    """
+    if rim_params is None:
+        rim_params = {}
+    if name is None:
+        # add random name
+        name = "generated" + str(random.randint(0, 1000000))
+
+    # normalize distributions to 100
+    for dim, dist in distributions.items():
+        total = sum(dist.values())
+        for k, v in dist.items():
+            dist[k] = v / total * 100
+
+    global_targets = []
+    for dim, dist in distributions.items():
+        global_targets.append({dim: dist})
+
+    schema = Rim(name, **rim_params)
+    schema.set_targets(
+        targets=global_targets, group_name="global group"
+    )
+    return schema
+
+def schema_from_df(
+        df: pd.DataFrame,
+        cols_weighting: List[str],
+        col_freq: str,
+        name: Optional[str] = None,
+        rim_params: Optional[Dict] = None
+) -> Rim:
+    """
+    Create a Rim schema from a dataframe.
+    The dataframe should have columns for the dimensions to be weighted,
+    and a column with the frequency of each row.
+
+    This function is useful for simple schemas, where filters are not needed. For more complex schemas,
+    use the Rim class directly.
+
+    Args:
+        df: Census dataframe
+        cols_weighting: Columns to be weighted by
+        col_freq: Column with the frequency of each row
+        name: Name of the schema
+        rim_params: Parameters for the Rim class
+
+    Returns:
+
+    """
+    distributions = {}
+    for col in cols_weighting:
+        dist = df[[col, col_freq]].groupby(col).sum()[col_freq].to_dict()
+        distributions[col] = dist
+
+    return schema_from_dict(
+        distributions=distributions,
+        name=name,
+        rim_params=rim_params
+    )
